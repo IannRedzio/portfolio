3d440f57e32da825b3ba2455d26ff51dc6307f03
IannRedzio <ianredzio13@gmail.com>
2021-09-10 20:39:06 -0300

    basic navbar

--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -2,7 +2,7 @@ import "./technologies.scss";
 
 export default function Technologies() {
     return (
-        <div className="technologies">
+        <div className="technologies" id="technologies">
             technologies
         </div>
     )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
 import "./topbar.scss";
+import MailIcon from "@material-ui/icons/Mail";
+import RoomIcon from "@material-ui/icons/Room";
 
 export default function Topbar() {
-    return (
-        <div className="topbar">
-            
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Logo
+          </a>
+          <div className="itemContainer">
+            <MailIcon className="icon" />
+            <span>ianredzio13@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <RoomIcon className="icon" />
+            <span>Buenos Aires, Argentina</span>
+          </div>
         </div>
-    )
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
