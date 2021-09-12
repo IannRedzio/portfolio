5f3a5ac854b0a8dbd5079e32a23ba8d87214c250
IannRedzio <ianredzio13@gmail.com>
2021-09-11 23:07:13 -0300

    add button menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Technologies from "./components/technologies/Technologies";
 import Topbar from "./components/topbar/Topbar";
 import Works from "./components/works/Works";
 import "./app.scss";
+import { useState } from "react";
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,9 +2,9 @@ import "./topbar.scss";
 import MailIcon from "@material-ui/icons/Mail";
 import RoomIcon from "@material-ui/icons/Room";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -19,7 +19,13 @@ export default function Topbar() {
             <span>Buenos Aires, Argentina</span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="menuButton" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
