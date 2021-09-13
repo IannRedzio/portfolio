55b0391c71a668dabe25f577cf693de068c42930
IannRedzio <ianredzio13@gmail.com>
2021-09-13 01:56:16 -0300

    landing with animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,41 @@
 import "./intro.scss";
+import HomeImage from "../../assets/imagehome.png";
+import LineDown from "../../assets/down.png";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay:  500,
+        backSpeed: 60,
+        strings: ["Developer", "Full Stack Developer", "Frontend Developer"],
+      });
+    }, []);
+
+
+
     return (
         <div className="intro" id="intro">
-            intro
+            <div className="left">
+                <div className="imageContainer">
+                    <img src={HomeImage} alt="homeimage" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hola, me llamo</h2>
+                    <h1>Ian Redzio</h1>
+                    <h3><span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src={LineDown} alt="linedown" />
+                </a>
+            </div>
         </div>
     )
 }
