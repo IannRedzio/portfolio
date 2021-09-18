b6980d7f96ca24fbe23ba4fd81599dc7f13d24f3
IannRedzio <ianredzio13@gmail.com>
2021-09-18 00:16:45 -0300

    add data for projects

---
+++ src/common/data.js
@@ -0,0 +1,28 @@
+const mainUrl="/videos"
+
+const data = [
+  {
+    id: "1",
+    title: "Videogames App",
+    desc: "Proyecto SPA(Single Page Aplication) de Stack PERN..",
+    url: "https://github.com/IannRedzio/PI-Videogames-FT14a",
+    urlPage: "https://github.com/IannRedzio/PI-Videogames-FT14a",
+    img: "https://user-images.githubusercontent.com/81378921/131145650-f02a94fc-d4ad-4868-8d17-7a38162ed40d.gif",
+  },
+  {
+    id: "2",
+    title: "Spotify Songs",
+    desc: "Proyecto SPA en el cual podemos reproducir y agregar como favoritos canciones de Spotify.",
+    url: "https://github.com/IannRedzio/PI-Videogames-FT14a",
+    img: "https://user-images.githubusercontent.com/81378921/131145650-f02a94fc-d4ad-4868-8d17-7a38162ed40d.gif",
+  },
+  {
+    id: "3",
+    title: "Nala",
+    urlPage: "https://nalarocks.com/",
+    desc: "Proyecto de Frontend desarrollado para NALA ROCKS como proyecto final para el bootcamp SoyHenry.",
+    img: mainUrl + "/Nala.png",
+  },
+];
+
+export default data;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,35 +3,12 @@ import arrowLeft from "../../assets/arrow-left.png";
 import arrowRight from "../../assets/arrow-right.png";
 import gitIcon from "../../assets/github2.png";
 import sitioWeb from "../../assets/sitio.png";
+import data from "../../common/data";
 import "./portfolio.scss";
 
 export default function Portfolio() {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const data = [
-    {
-      id: "1",
-      title: "Videogames App",
-      desc: "Proyecto SPA(Single Page Aplication) de Stack PERN..",
-      url: "https://github.com/IannRedzio/PI-Videogames-FT14a",
-      urlPage: "https://github.com/IannRedzio/PI-Videogames-FT14a",
-      img: "https://user-images.githubusercontent.com/81378921/131145650-f02a94fc-d4ad-4868-8d17-7a38162ed40d.gif",
-    },
-    {
-      id: "2",
-      title: "Spotify Songs",
-      desc: "Proyecto SPA en el cual podemos reproducir y agregar como favoritos canciones de Spotify.",
-      url: "https://github.com/IannRedzio/PI-Videogames-FT14a",
-      img: "https://user-images.githubusercontent.com/81378921/131145650-f02a94fc-d4ad-4868-8d17-7a38162ed40d.gif",
-    },
-    {
-      id: "3",
-      title: "Nala",
-      urlPage: "https://nalarocks.com/",
-      desc: "Proyecto de Frontend desarrollado para NALA ROCKS como proyecto final para el bootcamp SoyHenry.",
-      img: "https://user-images.githubusercontent.com/81378921/131145650-f02a94fc-d4ad-4868-8d17-7a38162ed40d.gif",
-    },
-  ];
 
   const handleClick = (way) => {
     way === "left"
@@ -64,7 +41,7 @@ export default function Portfolio() {
               </div>
               <div className="right">
                 <img
-                  src="https://user-images.githubusercontent.com/81378921/131145650-f02a94fc-d4ad-4868-8d17-7a38162ed40d.gif"
+                  src={d.img}
                   alt=""
                 />
               </div>
