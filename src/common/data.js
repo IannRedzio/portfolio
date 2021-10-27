d9d7a78840709f9c3bd05271aededf9a2dbfd2b9
IannRedzio <ianredzio13@gmail.com>
2021-10-27 19:59:29 -0300

    add todo project

--- src/common/data.js
+++ src/common/data.js
@@ -4,29 +4,37 @@ const data = [
   {
     id: "1",
     title: "Videogames App",
-    desc: "Proyecto SPA(Single Page Aplication) de Stack PERN..",
+    desc: "Proyecto SPA(Single Page Aplication) consumiendo la api de Rawg. Las tecnologías usadas son NodeJs, Express, Sequelize, PostgreSQL, React y Redux.",
     url: "https://github.com/IannRedzio/PI-Videogames-FT14a",
     urlPage: "https://github.com/IannRedzio/PI-Videogames-FT14a",
     img: "https://user-images.githubusercontent.com/81378921/131145650-f02a94fc-d4ad-4868-8d17-7a38162ed40d.gif",
   },
   {
     id: "2",
+    title: "Todo-List",
+    desc: "Aplicación hecha para agregar, eliminar y editar tareas. Usé Javascript junto a React.",
+    urlPage: "https://todo-app-one-pink.vercel.app/",
+    url: "https://github.com/IannRedzio/todo-app",
+    img: mainUrl + "/todo-app.png",
+  },
+  {
+    id: "3",
     title: "Spotify Songs",
     desc: "Proyecto SPA en el cual podemos reproducir y agregar como favoritos canciones de Spotify.",
     url: "https://github.com/IannRedzio?tab=repositories",
     img: mainUrl + "/spotify1.gif",
   },
   {
-    id: "3",
+    id: "4",
     title: "Nala",
     url: "https://nalarocks.com/",
     urlPage:
       "https://www.linkedin.com/in/ian-redzio/detail/treasury/position:1828527134/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADQxjJgBrXQRgx9ieRVU--lsPi-0Ny2IkNA%2C1635468428973)&section=position%3A1828527134&treasuryCount=1",
-    desc: "Proyecto de Frontend desarrollado para NALA ROCKS como proyecto final para el bootcamp SoyHenry.",
+    desc: "Proyecto de Frontend desarrollado para la empresa NALA ROCKS como proyecto final para el bootcamp SoyHenry.",
     img: mainUrl + "/Nala.png",
   },
   {
-    id: "4",
+    id: "5",
     title: "Expenses-React",
     urlPage: "https://expenses-react.vercel.app/",
     url: "https://github.com/IannRedzio/Expenses-practice-TS",
@@ -34,7 +42,7 @@ const data = [
     img: mainUrl + "/ExpensesApp.gif",
   },
   {
-    id: "5",
+    id: "6",
     title: "Quizz App",
     urlPage: "https://quizz-app-eight.vercel.app/",
     url: "https://github.com/IannRedzio/quizz-app",
@@ -42,7 +50,7 @@ const data = [
     img: mainUrl + "/Quizz.gif",
   },
   {
-    id: "6",
+    id: "7",
     title: "Google Homepage",
     urlPage: "https://google-clon-homepage.vercel.app/",
     url: "https://github.com/IannRedzio/google-homepage",
@@ -50,7 +58,7 @@ const data = [
     img: mainUrl + "/google.png",
   },
   {
-    id: "7",
+    id: "8",
     title: "Tesla Clone",
     urlPage: "https://tesla-clone-psi.vercel.app/#",
     url: "https://github.com/IannRedzio/tesla-clone",
